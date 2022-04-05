6a7a142fdd0bf02ffc670d76836a2788d33247bc
Davi Freitas <df828316@gmail.com>
2022-04-05 19:47:54 -0300

    corrigido conflito

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -27,39 +27,37 @@ class Login extends React.Component {
   render() {
     const { bttDisabled, inputPlayerName, inputGravatarEmail } = this.state;
     return (
-      <div>
-        <form>
-          <label htmlFor="input-player-name">
-            <input
-              id="input-player-name"
-              name="inputPlayerName"
-              data-testid="input-player-name"
-              onChange={ this.inputChange }
-              value={ inputPlayerName }
-              type="text"
-              placeholder="Digite seu nickname"
-            />
-          </label>
-          <label htmlFor="input-gravatar-email">
-            <input
-              id="input-gravatar-email"
-              name="inputGravatarEmail"
-              onChange={ this.inputChange }
-              value={ inputGravatarEmail }
-              data-testid="input-gravatar-email"
-              type="text"
-              placeholder="Digite seu email"
-            />
-          </label>
-          <button
-            disabled={ bttDisabled }
-            type="button"
-            data-testid="btn-play"
-          >
-            Play
-          </button>
-        </form>
-      </div>
+      <form>
+        <label htmlFor="input-player-name">
+          <input
+            id="input-player-name"
+            name="inputPlayerName"
+            data-testid="input-player-name"
+            onChange={ this.inputChange }
+            value={ inputPlayerName }
+            type="text"
+            placeholder="Digite seu nickname"
+          />
+        </label>
+        <label htmlFor="input-gravatar-email">
+          <input
+            id="input-gravatar-email"
+            name="inputGravatarEmail"
+            onChange={ this.inputChange }
+            value={ inputGravatarEmail }
+            data-testid="input-gravatar-email"
+            type="text"
+            placeholder="Digite seu email"
+          />
+        </label>
+        <button
+          disabled={ bttDisabled }
+          type="button"
+          data-testid="btn-play"
+        >
+          Play
+        </button>
+      </form>
     );
   }
 }
