91a6c3f4091643c0c808a680d59b25a07df8be47
Geovani Moura <94002157+GeovaniMoura@users.noreply.github.com>
2022-04-08 16:36:28 -0300

    Requisito 17 - Done

--- src/page/Ranking/index.jsx
+++ src/page/Ranking/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
   constructor() {
@@ -8,20 +9,37 @@ class Ranking extends React.Component {
 
   showRanking = () => {
     const ranking = JSON.parse(localStorage.getItem('ranking'));
-    console.log(ranking);
     return (
       <p data-testid="feedback-text">
         {ranking.name}
       </p>);
   };
 
+  redirectHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <h3 data-testid="ranking-title">Ranking</h3>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.redirectHome }
+        >
+          Voltar para a tela inicial
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Ranking;
